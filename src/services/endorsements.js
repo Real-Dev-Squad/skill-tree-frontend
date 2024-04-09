2992c7ae1683e84284f2e14f42d0f7c59e5fdd55
Hansraj8149 <hansrajsaini8149@gmail.com>
2024-04-09 11:47:36 +0530

    updated fetch with axios

--- src/services/endorsements.js
+++ src/services/endorsements.js
@@ -1,13 +1,14 @@
 import { useQuery, useMutation } from "@tanstack/react-query";
 import { BASE_URL } from "@/constants/urls";
+import axios from 'axios'
 export const useGetEndorsements = () =>
     useQuery({
         queryKey: ["endorsements"],
-        queryFn: () => fetch(`${BASE_URL}/endorsements`).then((res) => res.json()),
+        queryFn: () => axios.get(`${BASE_URL}/endorsements`).then((res) => res.json()),
     });
 
 export const usePostEndorsement = ({ endorsementData }) =>
     useMutation({
         mutationKey: ["endorsements"],
-        mutationFn: () => fetch(`${BASE_URL}/endorsements`, endorsementData).then((res) => res.json()),
+        mutationFn: () => axios.post(`${BASE_URL}/endorsements`, endorsementData).then((res) => res.json()),
     });
