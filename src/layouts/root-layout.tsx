49a14c3b6da2deadf7616a6a192934c2f4051fff
yash raj <yashraj0207@outlook.com>
2024-08-04 21:37:13 +0530

    add warning for small screen devices

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -1,3 +1,4 @@
+import { SmallScreenWarning } from "@/components/small-screen-warning"
 import { Navbar } from "@/components/navbar"
 
 type Props = {
@@ -8,7 +9,7 @@ export const RootLayout = ({ children }: Props) => {
     return (
         <div className="flex min-h-screen flex-col">
             <Navbar />
-
+            <SmallScreenWarning />
             <main className="mt-14 flex flex-1 flex-col">{children}</main>
         </div>
     )
