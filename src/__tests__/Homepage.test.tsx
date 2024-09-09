b186442992b369b5770e19204b7765253bb17734
Ranita Saha <saharanitaa1230@gmail.com>
2024-09-09 20:57:29 +0530

    updated documentation

--- src/__tests__/Homepage.test.tsx
+++ src/__tests__/Homepage.test.tsx
@@ -3,6 +3,7 @@ import { expect, test } from "vitest"
 
 import Homepage from "@/pages"
 
+//TODO: Needs modification
 test("HomePage", () => {
     render(<Homepage />)
     expect(screen.getByRole("heading", { level: 1, name: "Welcome to Skilltree" })).toBeDefined()
