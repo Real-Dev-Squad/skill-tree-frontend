5e1463c9c2d8f0a0f0778af78f615833c3437e52
Ranita Saha <saharanitaa1230@gmail.com>
2024-09-11 08:30:03 +0530

    updated documentation

--- src/__tests__/Homepage.test.tsx
+++ src/__tests__/Homepage.test.tsx
@@ -3,7 +3,9 @@ import { expect, test } from "vitest"
 
 import Homepage from "@/pages"
 
-//TODO: Needs modification
+// TODO: Requires modification:
+// 1. Only the heading tag has been added to verify that the tests are working.
+// 2. Once the tests are set up, verify other cases.
 test("HomePage", () => {
     render(<Homepage />)
     expect(screen.getByRole("heading", { level: 1, name: "Welcome to Skilltree" })).toBeDefined()
