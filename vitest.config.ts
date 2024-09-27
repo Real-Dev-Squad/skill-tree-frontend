ef24ebe83f9a71c029ab8af01899f8fd7e8d6c99
Sunil Kumar <sunilkumar24.dev@gmail.com>
2024-09-27 18:16:37 +0500

    documentation updated for test:coverage cmd

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,19 +8,19 @@ export default defineConfig({
     test: {
         environment: "jsdom",
         coverage: {
-            include: ['src/**/*.{ts,tsx}'], 
+            include: ["src/**/*.{ts,tsx}"],
             exclude: [
-                'src/config.ts',
-                'src/routes.ts',
-                'src/enums/**',
-                'src/store/**',
-                'src/styles/**',
-                'src/pages/**',  
-                'src/api/**',    
-                'src/__tests__/**',  
-                'node_modules/**',  
+                "src/config.ts",
+                "src/routes.ts",
+                "src/enums/**",
+                "src/store/**",
+                "src/styles/**",
+                "src/pages/**",
+                "src/api/**",
+                "src/__tests__/**",
+                "node_modules/**",
             ],
-        }
+        },
     },
     resolve: {
         alias: {
